b101c0eded6a66fb69bd2cf8518b260746c839d6
Ashwin Naren <arihant2math@gmail.com>
2023-04-01 17:27:53 -0700

    added logo

--- conf.py
+++ conf.py
@@ -52,7 +52,11 @@ html_theme = 'furo'
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+html_static_path = ["_static"]
+html_theme_options = {
+    "light_logo": "Logo.png",
+    "dark_logo": "Logo.png",
+}
 
 html_show_sphinx = False
 html_theme_options = {
