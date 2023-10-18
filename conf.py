6ad93f9a2ddcb9301e0d38dd5027b70c0b02b7df
Ashwin Naren <arihant2math@gmail.com>
2023-10-18 16:03:30 -0700

    revamped javadoc

--- conf.py
+++ conf.py
@@ -10,9 +10,10 @@
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath("."))
 
 
 # -- Project information -----------------------------------------------------
@@ -31,7 +32,12 @@ release = ''
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
+    "java5206"
 ]
+
+javadoc_url_map = {
+    # 'org.firstinspires.ftc.Vision': ('https://javadoc.io/doc/org.firstinspires.ftc/Vision/', 'javadoc'),
+}
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
